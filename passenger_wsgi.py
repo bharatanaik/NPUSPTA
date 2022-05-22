e28b58d6273f0b43663153f591527dd9cf33e80f
Bharat Naik <bharat.anaik2003@gmail.com>
2022-05-22 19:47:32 +0530

    myruyuy

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,1 +1,4 @@
-from core.wsgi import application
+import os
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+application = get_wsgi_application()
