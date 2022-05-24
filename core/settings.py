d43c1fce32ec62d9a11b70dc543455699a41d79c
Bharat Naik <bharat.anaik2003@gmail.com>
2022-05-24 20:28:28 +0530

    settings

--- core/settings.py
+++ core/settings.py
@@ -64,7 +64,10 @@ DATABASES = {
         'USER': data_user,
         'PASSWORD': data_pass,
         'HOST':'localhost',
-        'PORT':3306
+        'PORT':3306,
+        'OPTIONS':{
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
