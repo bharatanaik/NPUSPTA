8610c862906a8ab72db085c525f1676e6aff2c19
SERIOUS VIPER <93201030+serious-viper@users.noreply.github.com>
2022-06-26 22:32:17 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -54,7 +54,24 @@ TEMPLATES = [
         },
     },
 ]
-
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
 WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
